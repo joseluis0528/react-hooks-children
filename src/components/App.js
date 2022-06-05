b039cfa5cf6ef1906708d0727f04ba982098a63d
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2022-06-04 19:43:24 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+        <div>This is #1 example!</div>
+        <div>This is #2 example!</div>
+        <div>This is #3 example!</div>
+        <div>This is #4 example!</div>
+        <div>This is #5 example!</div>
+        <div>This is #6 example!</div>
+        <div>This is #7 example!</div>
       </Container>
       <Container header="Example header" textPosition="center">
         <img
